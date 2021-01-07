16c12130be05eb3687d65301f37dea41f60f9d15
Brian <brbuxton@cisco.com>
2021-01-07 11:34:48 -0500

    Progress on Step 2 - compose an access policy and rule

--- blackhole.py
+++ blackhole.py
@@ -9,12 +9,16 @@ The script looks for several OS environment variables: FMCHOST (ip address of th
 FMCPORT (port number of the FMC), FMCADMIN (FMC admin account name), FMCPASS (password for
 the FMC admin account).  Please also identify the specific rule ID and name for the
 exception traffic to the black hole.
+
+Code to format FMC requests was pulled from:
+https://github.com/CiscoDevNet/dne-security-code/blob/master/intro-firepower/fmc/fmc_requests.py
 """
 
 import os
 import requests.auth
 
-fmc = f"https://{os.getenv('FMCHOST')}:{os.getenv('FMCPORT')}/api/fmc_platform/v1/"
+fmc_platorm = f"https://{os.getenv('FMCHOST')}:{os.getenv('FMCPORT')}/api/fmc_platform/v1"
+fmc_config = f"https://{os.getenv('FMCHOST')}:{os.getenv('FMCPORT')}/api/fmc_config/v1"
 domain_uuid = ""
 headers = {
     "Content-Type": "application/json",
@@ -22,12 +26,12 @@ headers = {
 }
 access_policy = {
     "type": "AccessPolicy",
-    "name": "DNE Security Access Control Policy",
+    "name": "Example Security Access Control Policy",
     "description": "Basic AC Policy",
     "defaultAction": {"action": "BLOCK"},
 }
 access_rule = {
-    "action": "ALLOW", # Any exception to the black hole?
+    "action": "ALLOW",  # Any exception to the black hole?
     "enabled": True,
     "type": "AccessRule",
     "name": "Rule1",
@@ -51,7 +55,8 @@ access_rule = {
 def authentication():
     # Get a token and the domain UUID
     basicauth = requests.auth.HTTPBasicAuth(os.getenv('FMCADMIN'), os.getenv('FMCPASS'))
-    response = requests.post(fmc + "auth/generatetoken", headers=headers, auth=basicauth, verify=False)
+    response = requests.post(fmc_platorm + "/auth/generatetoken", headers=headers, auth=basicauth, verify=False)
+    global domain_uuid
     if response.status_code == 200:
         access_token = response.headers.get("X-auth-access-token")
         domain_uuid = response.headers.get("DOMAIN_UUID")
@@ -60,3 +65,42 @@ def authentication():
     else:
         print(f"Non-200 response encountered! - {response.status_code}")
     return access_token, domain_uuid
+
+
+def create_url(endpoint_path, host=FMC.get("host"), port=FMC.get("port")):
+    """Create an FMC configuration API endpoint URL.  Copied from DevNET."""
+    return fmc_config + f"/domain/{domain_uuid}/{endpoint_path}"
+
+
+def fmc_post(endpoint_path, data):
+    """Send a POST request to FMC and return the parsed JSON response.  Copied from DevNET."""
+    url = create_url(endpoint_path)
+
+    print("Sending POST request to", url)
+    response = requests.post(url, headers=headers, json=data, verify=False)
+    response.raise_for_status()
+
+    return response.json()
+
+
+def fmc_get(endpoint_path):
+    """Send a GET request to FMC and return the parsed JSON response.  Copied from DevNET."""
+    url = create_url(endpoint_path)
+
+    print("Sending GET request to", url)
+    response = requests.get(url, headers=headers, verify=False)
+    response.raise_for_status()
+
+    return response.json()
+
+
+def fmc_delete(endpoint_path):
+    """Send a DELETE request to FMC and return the parsed JSON response.  Copied from DevNET."""
+    url = create_url(endpoint_path)
+
+    print("Sending DELETE request to", url)
+    response = requests.delete(url, headers=headers, verify=False)
+    response.raise_for_status()
+
+    return response.json()
+
